524a6b3aab2e3db6efbcbda746316c070cd1869b
Fro Zen <38363715+Frozenreflex@users.noreply.github.com>
2022-05-22 03:06:26 -0500

    Init

---
+++ AudioCustomizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NeosModLoader;
+using UnityEngine;
+
+namespace AudioCustomizer
+{
+    public class AudioCustomizer : NeosMod
+    {
+        public override string Name => "AudioCustomizer";
+        public override string Author => "Fro Zen";
+        public override string Version => "1.0.0";
+
+        private static bool _first_trigger = false;
+        private static ModConfiguration _config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<int> VoicesKey =
+            new ModConfigurationKey<int>("Voice Count", "Audio Voices (Requires Restart)", () => 64);
+
+        public override void OnEngineInit()
+        {
+            var a = AudioSettings.GetConfiguration();
+            _config = GetConfiguration();
+            a.numRealVoices = _config.GetValue(VoicesKey);
+            AudioSettings.Reset(a);
+            //this completely screws up the audio
+            /*
+            GetConfiguration().OnThisConfigurationChanged += config => 
+            {
+                var audio = AudioSettings.GetConfiguration();
+                audio.numRealVoices = _config.GetValue(VoicesKey);
+                AudioSettings.Reset(audio);
+                Msg($"Audio voice count set to {audio.numRealVoices}");
+            };
+            */
+        }
+    }
+}
